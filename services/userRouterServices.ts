18e01a8a7c9a0428eb689e9e7cd71d8ff1412f75
otdot <otto.mursula@edu.bc.fi>
2022-11-20 21:34:52 +0200

    refactored product and user post router functions

--- services/userRouterServices.ts
+++ services/userRouterServices.ts
@@ -12,36 +12,38 @@ export const createUser = (req: Request, res: Response, next: NextFunction) => {
       //this return below might cause an error, because its not returning something in every possible case.
       if (response) {
         res.status(400).json({ error: "name must be unique" });
-        return;
+      } else {
+        console.log("Username OK");
+        bcrypt
+          .hash(user.passwordHash, 10)
+          .then((hashedpass) => {
+            const newUser = new User({
+              name: user.name,
+              passwordHash: hashedpass,
+              position: user.position,
+            });
+
+            newUser
+              .save()
+              .then(() => {
+                console.log(`new user ${newUser.name} created`);
+                res.status(200).json(newUser);
+              })
+              .catch((err: unknown) => {
+                console.log(`Something went wrong(userRouter.post: /): ${err}`);
+                next();
+              });
+          })
+          .catch((err) => res.status(400).send(`couldnt save user ${err}`));
       }
-      console.log("Username OK");
     })
     .catch((err) => {
-      //later add next func here
-      console.log(`Something went wrong(userRouter.post: /): ${err}`);
+      res
+        .status(400)
+        .send(
+          `Couldn't find products. Something went wrong(productRouter.post: /): ${err}`
+        );
     });
-
-  bcrypt
-    .hash(user.passwordHash, 10)
-    .then((hashedpass) => {
-      const newUser = new User({
-        name: user.name,
-        passwordHash: hashedpass,
-        position: user.position,
-      });
-
-      newUser
-        .save()
-        .then(() => {
-          console.log(`new user ${newUser.name} created`);
-          res.status(200).json(newUser);
-        })
-        .catch((err: unknown) => {
-          console.log(`Something went wrong(userRouter.post: /): ${err}`);
-          next();
-        });
-    })
-    .catch((err) => res.status(400).send(`couldnt save user ${err}`));
 };
 
 export const updateWorkingPlace = (
